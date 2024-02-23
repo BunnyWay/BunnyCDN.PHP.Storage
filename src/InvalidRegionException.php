beacd3d11e0ee2673c55adf33c391a11726c7a29
Rafael Kassner <rafael@bunny.net>
2024-02-23 14:45:30 +0100

    Update php-cs-fixer to 3.49.0

--- src/InvalidRegionException.php
+++ src/InvalidRegionException.php
@@ -6,7 +6,7 @@ namespace Bunny\Storage;
 
 class InvalidRegionException extends Exception
 {
-    public function __construct(int $code = 0, \Exception $previous = null)
+    public function __construct(int $code = 0, ?\Exception $previous = null)
     {
         parent::__construct('Invalid storage region', $code, $previous);
     }
